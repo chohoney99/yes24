f14a995136fc9e0ee5251a00c66e43211509feb2
chohoney99 <chohoney99@naver.com>
2023-09-06 17:34:10 +0900

    230906

--- js/js.js
+++ js/js.js
@@ -4,3 +4,20 @@ $(".mypage").mouseover(function(){
 $(".mypagebox").mouseout(function(){
     $(this).hide();
 });
+
+$("#quick").mouseover(function(){
+    $("#mainlist").show();
+});
+$("#mainlist").mouseout(function(){
+    $(this).hide();
+});
+$("#quick").mouseout(function(){
+    $("#mainlist").hide();
+});
+
+$("#mainlist li").mouseover(function(){
+    $(this).children(".mainlistbox").show();
+});
+$("#mainlist li").mouseout(function(){
+    $(this).children(".mainlistbox").hide();
+});
