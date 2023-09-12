f6e9c40ed2d3bc572252a2c04468121c82402cd9
chohoney99 <chohoney99@naver.com>
2023-09-12 17:33:45 +0900

    230912

--- js/main.js
+++ js/main.js
@@ -27,10 +27,48 @@ $(".contents1box2 ul:nth-of-type(2) li:nth-of-type(2)").hover(function(){
 $.ajax({
     method: "GET",
     url: "https://dapi.kakao.com/v3/search/book?target=title",
-    data: {query: "고통에 관하여"},
+    data: {query: "도시와 그 불확실한 벽"},
     async: false,
     headers: {Authorization: "KakaoAK d6ea9c3bec21e99e300ae669cdaa3d50"}
 })
     .done(function(msg){
-        $("#contents2mainbox").append("<img src='" + msg.documents[0].thumbnail + "'>");
+        $("#contents2mainboxthumb").append("<img src='" + msg.documents[0].thumbnail + "'>");
+        $("#contents2mainboxtext p:nth-of-type(2)").append(msg.documents[0].title);
+        $("#contents2mainboxpre").append("<img src='" + msg.documents[0].thumbnail + "'>");
+        $("#ranking li:nth-of-type(1)").append("<img src='" + msg.documents[0].thumbnail + "'>");
+        $("#ranking li:nth-of-type(1)").append("<p>" + msg.documents[0].title + "</p>");
+        $("#ranking li:nth-of-type(1)").append("<p>" + msg.documents[0].authors + "</p>");
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {query: "최민준의 아들코칭 백과"},
+    async: false,
+    headers: {Authorization: "KakaoAK d6ea9c3bec21e99e300ae669cdaa3d50"}
+})
+    .done(function(msg){
+        $("#contents2mainboxpre").append("<img src='" + msg.documents[0].thumbnail + "'>");
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {query: "멘탈을 회복하는 연습"},
+    async: false,
+    headers: {Authorization: "KakaoAK d6ea9c3bec21e99e300ae669cdaa3d50"}
+})
+    .done(function(msg){
+        $("#contents2mainboxpre").append("<img src='" + msg.documents[0].thumbnail + "'>");
+    });
+
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {query: "삶은 예술로 빛난다"},
+    async: false,
+    headers: {Authorization: "KakaoAK d6ea9c3bec21e99e300ae669cdaa3d50"}
+})
+    .done(function(msg){
+        $("#contents2mainboxpre").append("<img src='" + msg.documents[0].thumbnail + "'>");
     });
